1c841ef32185d6ca77611a2c51709026b7b3c994
andrespicazo <andrespm02@gmail.com>
2024-12-05 10:28:54 +0100

    Creacion de carpeta

---
+++ utility/Tupla.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class Tupla<X, Y> { 
+    public X first; 
+    public Y second; 
+
+    public Tupla(X x, Y y) { 
+      this.first = x; 
+      this.second = y; 
+    } 
+}
